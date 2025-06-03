48a794c70cf78f716be19d2db416ce093bc77c1c
Danielcore094 <daniel.rodriguez04@hotmail.com>
2025-06-02 19:08:14 -0500

    Update function.routes.js

--- src/routes/function.routes.js
+++ src/routes/function.routes.js
@@ -3,6 +3,8 @@ import { crearUsuario, consultarUsuarios, loginUsuario, eliminarUsuario } from "
 import { resultadoValidacion } from "../../helpers/validateHelper.js";
 import { validarCrear, validarCliente } from "../../middleware/validation/validation.js";
 import { crearCliente } from "../controllers/cliente.controller.js";
+import { actualizarCliente } from "../controllers/cliente.controller.js"; 
+import { loginCliente } from "../controllers/cliente.controller.js";
 
 const router = Router();
 
@@ -10,7 +12,9 @@ const router = Router();
 router.post("/usuarios", validarCrear, resultadoValidacion, crearUsuario);
 router.get("/usuarios", consultarUsuarios);
 router.delete("/usuarios/:id", eliminarUsuario);
-router.post("/login", loginUsuario);
+router.post("/loginUsuario", loginUsuario);
 router.post("/clientes", validarCliente, resultadoValidacion, crearCliente);
+router.put("/clientes/:id", validarCliente, resultadoValidacion, actualizarCliente); 
+router.post("/loginCliente", loginCliente); 
 
 export default router;
